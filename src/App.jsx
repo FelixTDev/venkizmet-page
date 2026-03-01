0b5af1231b321e6a4892167e0bcdfef29f81b2ae
Felix <salimv2305@gmail.com>
2026-03-01 14:45:49 -0500

    Prueba en pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import { HashRouter as BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import { AnimatePresence } from 'framer-motion'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
